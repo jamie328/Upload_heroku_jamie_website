a9ef1c47003355565ecbf3b61997a9cd56df88a1
HuangJamison <b10130402@gmail.com>
2019-09-28 00:54:42 +0800

    github-auth finish

--- web_create_project/settings.py
+++ web_create_project/settings.py
@@ -28,14 +28,19 @@ LOGOUT_URL = '/logout/'
 AUTHENTICATION_BACKENDS = [
         'django.contrib.auth.backends.ModelBackend',  # 預設
         'social_core.backends.google.GoogleOAuth2',   # 新增於9/27
+        'social_core.backends.github.GithubOAuth2',   # 新增於9/27
 ]
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 # localhost key secret
 # SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '191042244574-rcvbcumjt3tmhtcetnealn89co1fmbli.apps.googleusercontent.com'
 # SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'ZNb2y_niK1mYkXAg38Wsnnut'
+# SOCIAL_AUTH_GITHUB_KEY = 'b8c658e7ee2d2bc9dc03'
+# SOCIAL_AUTH_GITHUB_SECRET = '83be6a407ca7b1051e62384c2fbe4809648b279b'
 # heroku key secret
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '191042244574-3fsaenguf469lhde81a5493usk27hrjo.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '9SrlkIlZ2jztLbeRgCjTveTE'
+SOCIAL_AUTH_GITHUB_KEY = '882927535ceb6c029118'
+SOCIAL_AUTH_GITHUB_SECRET = 'cd014409817cd5039c4a175fa019c9a41246b074'
 """9/27 新增 google API第三方"""
 
 
